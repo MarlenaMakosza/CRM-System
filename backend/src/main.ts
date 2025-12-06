b23769971670fe9d3af14e1ba347cf434e743c24
Marlena Mąkosza <marlenamakosza@gmail.com>
2025-12-06 21:57:42 +0100

    testing connection with database - main.ts

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,6 +1,14 @@
+import { sql } from "../../db/db.ts";
+
+
 export function add(a: number, b: number): number {
   return a + b;
 }
 
 // Learn more at https://docs.deno.com/runtime/manual/examples/module_metadata#concepts
 console.log("Add 2 + 3 =", add(2, 3));
+
+const result = await sql`
+  SELECT ID, IMIE FROM KLIENT;
+`;
+console.log(result);
